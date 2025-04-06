661eb04bae64f981cc8921842542b6681cb27a28
proff001 <thommhole@gmail.com>
2025-04-06 22:51:31 +0200

    Used double underscore by accident

--- graphics.py
+++ graphics.py
@@ -2,28 +2,28 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
 	def __init__(self, width, height):
-		self.__root = Tk()
-		self.__root.title("Maze Solver")
-		self.__root.protocol("WM_DELETE_WINDOW", self.close)
-		self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
-		self.__canvas.pack(fill=BOTH, expand=1)
-		self.__running = False
+		self._root = Tk()
+		self._root.title("Maze Solver")
+		self._root.protocol("WM_DELETE_WINDOW", self.close)
+		self._canvas = Canvas(self._root, bg="white", width=width, height=height)
+		self._canvas.pack(fill=BOTH, expand=1)
+		self._running = False
 	
 	def redraw(self):
-		self.__root.update_idletasks()
-		self.__root.update()
+		self._root.update_idletasks()
+		self._root.update()
 	
 	def wait_for_close(self):
-		self.__running = True
-		while self.__running:
+		self._running = True
+		while self._running:
 			self.redraw()
 		print("window closed...")
 
 	def close(self):
-		self.__running = False
+		self._running = False
 
 	def draw_line(self, line, color="black"):
-		line.draw(self.__canvas, color)
+		line.draw(self._canvas, color)
 
 class Point:
 	def __init__(self, x, y):
